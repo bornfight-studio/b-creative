8bb3a16ca5b63f688550924e7757e492d7c79c37
ijurko <ijurko19@gmail.com>
2020-04-29 13:09:16 +0200

    ScrollTo console.error fix

--- generators/app/templates/static/js/components/ScrollTo.js
+++ generators/app/templates/static/js/components/ScrollTo.js
@@ -6,65 +6,63 @@ import gsap from "gsap";
  * Optionally link can have data-offset
  */
 export class ScrollTo {
-    constructor() {
-        /**
-         * ScrollTo DOM selector
-         * ScrollTo dataset
-         */
+  constructor() {
+    /**
+     * ScrollTo DOM selector
+     * ScrollTo dataset
+     */
 
-        this.DOM = {
-            link: ".js-scroll-to",
-        };
-        /**
-         * default amount of pixels to scroll from top if no data-offset was set
-         * @type {number}
-         */
-        this.defaultOffset = 0;
+    this.DOM = {
+      link: ".js-scroll-to",
+    };
+    /**
+     * default amount of pixels to scroll from top if no data-offset was set
+     * @type {number}
+     */
+    this.defaultOffset = 0;
 
-        /**
-         * fetch link elements DOM element
-         * @type {NodeListOf<Element>}
-         */
-        this.links = document.querySelectorAll(this.DOM.link);
+    /**
+     * fetch link elements DOM element
+     * @type {NodeListOf<Element>}
+     */
+    this.links = document.querySelectorAll(this.DOM.link);
+  }
+
+  init() {
+    if (this.links.length !== null) {
+      this.scrollToInit();
+    } else {
+      console.error(
+        `${this.DOM.link} with dataset ${this.DOM.dataset.target} does not exist in the DOM!`
+      );
     }
+  }
 
-    init() {
-        if (this.links.length !== null) {
-            this.scrollToInit();
-        } else {
-            console.error(
-                `${this.DOM.link} with dataset ${this.DOM.dataset.target} does not exist in the DOM!`,
-            );
+  scrollToInit() {
+    this.links.forEach((link) => {
+      if (link.hasAttribute("data-target")) {
+        const target = document.querySelector(`${link.dataset.target}`);
+        let offset = -parseInt(link.dataset.offset);
+        if (!target) {
+          return;
         }
-    }
+        if (!offset) {
+          offset = this.defaultOffset;
+        }
 
-    scrollToInit() {
-        this.links.forEach((link) => {
-            if (link.hasAttribute("data-target")) {
-                const target = document.querySelector(`${link.dataset.target}`);
-                let offset = -parseInt(link.dataset.offset);
-                if (!target) {
-                    return;
-                }
-                if (!offset) {
-                    offset = this.defaultOffset;
-                }
-
-                link.addEventListener("click", () => {
-                    gsap.to([document.documentElement, document.body], 1, {
-                        scrollTop:
-                            -document.body.getBoundingClientRect().y +
-                            target.getBoundingClientRect().y +
-                            offset,
-                        ease: "power3",
-                    });
-                });
-            } else {
-                console.error(link);
-                console.error(
-                    `link is missing data-${this.DOM.dataset.target}`,
-                );
-            }
+        link.addEventListener("click", () => {
+          gsap.to([document.documentElement, document.body], 1, {
+            scrollTop:
+              -document.body.getBoundingClientRect().y +
+              target.getBoundingClientRect().y +
+              offset,
+            ease: "power3",
+          });
         });
-    }
+      } else {
+        console.error(link);
+        console.error(`link is missing data-target`);
+      }
+    });
+  }
 }
