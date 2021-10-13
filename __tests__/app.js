6529aecceeec192ab8fa844e40036927f3bff9c6
marioloncarek <mario.loncarek@gmail.com>
2021-10-13 15:38:19 +0200

    b-creative - update deps, format, new sass, husky prepare

--- __tests__/app.js
+++ __tests__/app.js
@@ -4,11 +4,7 @@ const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
 describe('b-creative:app', () => {
-  beforeAll(() => {
-    return helpers
-      .run(path.join(__dirname, '../generators/app'))
-      .withPrompts({ someAnswer: true });
-  });
+  beforeAll(() => helpers.run(path.join(__dirname, '../generators/app')).withPrompts({ someAnswer: true }));
 
   it('creates files', () => {
     assert.file(['dummyfile.txt']);
