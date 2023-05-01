2ac967265a96fb1c8b67541760c2787771cbd1ad
Darko Komerički <darko.komericki@bornfight.com>
2023-05-01 20:48:00 +0200

    added comments to webpack config

--- generators/app/templates/common/webpack.config.js
+++ generators/app/templates/common/webpack.config.js
@@ -1,3 +1,4 @@
+// Import required modules
 const path = require("path");
 const webpack = require("webpack");
 const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
@@ -9,8 +10,10 @@ module.exports = (env, argv) => {
     console.log(`Running in ${mode} mode...`);
     console.log(env);
 
-    const proxy = "http://<%= vhost %>";
+    // Define the proxy URL for BrowserSync
+    const proxy = "http://<%= vhost %>/";
 
+    // Define the entry points for the webpack build
     const entry = {
         bundle: "./static/js/index.js",
         <%_ if (react) { _%>
@@ -19,7 +22,9 @@ module.exports = (env, argv) => {
         style: ["./static/scss/style.scss"],
     };
 
+    // Define the plugins to be used in the webpack build
     const plugins = [
+        // Start BrowserSync and reload the page when files change
         new BrowserSyncPlugin(
             {
                 proxy: proxy,
@@ -31,17 +36,21 @@ module.exports = (env, argv) => {
                 reload: false,
             },
         ),
+        // Remove empty script tags from the JS output
         new RemoveEmptyScriptsPlugin({ verbose: true }),
+        // Extract CSS into separate files
         new MiniCssExtractPlugin({
             filename: "[name].css",
         }),
         <%_ if (react) { _%>
+        // Provide React globally so it can be used without importing it
         new webpack.ProvidePlugin({
             React: "react",
         }),
         <%_ } _%>
     ];
 
+    // Add a source map for easier debugging in development mode
     if (mode === "development") {
         plugins.push(
             new webpack.SourceMapDevToolPlugin({
@@ -50,49 +59,63 @@ module.exports = (env, argv) => {
         );
     }
 
+    // Define the webpack configuration object
     return {
+        // Define the entry points from `entry` variable
         entry: entry,
+        // Define the output directory and file names
         output: {
             filename: "[name].js",
             path: path.resolve(__dirname, "static", "dist"),
             clean: true,
         },
         <%_ if (react) { _%>
-            resolve: { extensions: [".js", ".jsx"] },
+        // Add support for importing .jsx files in addition to .js files
+        resolve: { extensions: [".js", ".jsx"] },
         <%_ } else { _%>
-            resolve: { extensions: [".js"] },
+        // Add support for importing only .js files
+        resolve: { extensions: [".js"] },
         <%_ } _%>
+        // Define the plugins to be used from `plugins` variable
         plugins: plugins,
         module: {
             rules: [
                 {
                     <%_ if (react) { _%>
+                    // Handle .js and .jsx files with babel-loader
                     test: /\.(js|jsx)$/,
                     <%_ } else { _%>
+                    // Handle .js files with babel-loader
                     test: /\.(js)$/,
                     <%_ } _%>
                     exclude: /node_modules/,
                     use: ["babel-loader"],
                 },
                 {
+                    // Handle .scss files with sass-loader, css-loader, and MiniCssExtractPlugin
                     test: /\.s[ac]ss$/i,
                     use: [
+                        // Extract CSS into separate file
                         MiniCssExtractPlugin.loader,
                         {
                             loader: "css-loader",
                             options: {
+                                // Disable URL handling in CSS
                                 url: false,
                             },
                         },
+                        // Compile SCSS to CSS
                         "sass-loader",
                     ],
                 },
                 {
+                    // Match .css files
                     test: /\.css$/i,
                     use: [
                         {
                             loader: "css-loader",
                             options: {
+                                // Disable URL handling in CSS
                                 url: false,
                             },
                         },
@@ -100,16 +123,30 @@ module.exports = (env, argv) => {
                 },
             ],
         },
+        // https://webpack.js.org/plugins/split-chunks-plugin/#optimizationsplitchunks
         optimization: {
-            // https://webpack.js.org/plugins/split-chunks-plugin/#optimizationsplitchunks
             splitChunks: {
-                chunks: "all",
-                minSize: 20000,
-                minRemainingSize: 0,
-                minChunks: 1,
-                maxAsyncRequests: 30,
-                maxInitialRequests: 30,
-                enforceSizeThreshold: 50000,
+                chunks: "all", // Optimization should be applied to all chunks, including the entry chunk and the chunks created by import().
+                minSize: 20000, // Minimum size in bytes of a chunk before it can be split. In this case, a chunk must be at least 20 kilobytes before it can be split.
+                minRemainingSize: 0, // Minimum size in bytes of the remaining chunk after splitting. If the remaining chunk is smaller than this size, it won't be split. In this case, there is no minimum size requirement.
+                minChunks: 1, // Minimum number of chunks that must share a module before the module is split into a separate chunk. In this case, any module that is used in at least one chunk will be split into a separate chunk.
+                maxAsyncRequests: 30, // Maximum number of parallel requests for the chunks that are loaded asynchronously. In this case, a maximum of 30 parallel requests will be made.
+                maxInitialRequests: 30, // Maximum number of parallel requests for the chunks that are loaded on the initial page load. In this case, a maximum of 30 parallel requests will be made.
+                enforceSizeThreshold: 50000, // Threshold size in bytes for enforcing the minRemainingSize option. If the remaining chunk after splitting is larger than this size, the minRemainingSize option will be enforced. In this case, the threshold is 50 kilobytes.
+                /**
+                 * `cacheGroups` is an Object that allows you to group chunks
+                 * and customize their behavior. The vendor cache group is
+                 * used to group all modules from the node_modules folder into
+                 * a single chunk, which is named vendor. The default cache
+                 * group is used to group modules that don't belong to the
+                 * node_modules folder into a separate chunk, which is named
+                 * based on the chunk's ID. Both cache groups have a priority
+                 * option that determines the order in which they are
+                 * processed.
+                 *
+                 * The reuseExistingChunk option is set to true for both cache
+                 * groups to allow them to reuse existing chunks when possible.
+                 */
                 cacheGroups: {
                     vendor: {
                         test: /[\\/]node_modules[\\/]/,
