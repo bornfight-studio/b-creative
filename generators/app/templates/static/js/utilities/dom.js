7a1092f49d0d59d9af9d388d0974d8017dc913a0
tomo <tjezidzic@outlook.com>
2024-03-11 12:10:14 +0100

    comments fix

--- generators/app/templates/static/js/utilities/dom.js
+++ generators/app/templates/static/js/utilities/dom.js
@@ -1,6 +1,28 @@
+/**
+ * DOM utilities
+ * usage
+ * import { $html, $body } from "../utilities/dom.js";
+ */
+
+/**
+ * Get html element
+ * @type {HTMLElement}
+ */
 const $html = document.documentElement
+
+/**
+ * Get document body
+ * @type {HTMLElement}
+ */
 const $body = document.body
 
+/**
+ * DOM utilities export
+ * @type {{
+ * $html: HTMLElement,
+ * $body: HTMLElement
+ * }}
+ */
 export {
     $html,
     $body,
