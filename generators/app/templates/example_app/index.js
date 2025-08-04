91d6544c65906ab66aa36afd4bcfaeac10592bd3
Darko Komerički <darko.komericki@bornfight.studio>
2025-08-04 14:27:20 +0200

    generate manifest file (#75)
    * generate manifest file
    
    output is /static/dist/manifest.json
    
    * remove react and add contenthash only when webpack is in production mode

--- generators/app/templates/example_app/index.js
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-
-const domNode = document.getElementById("root");
-
-if (domNode !== null) {
-    const root = createRoot(domNode);
-    root.render(<App />);
-}
